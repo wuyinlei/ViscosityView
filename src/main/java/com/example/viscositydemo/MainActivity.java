6a72cf18bf06867e4a7a0a56366f16309bf43093
wuyinlei <1069584784@qq.com>
2016-02-04 14:17:03 +0800

    完成高仿未读消息的小球的绘制和移动

---
+++ src/main/java/com/example/viscositydemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.viscositydemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new ViscosityView(this));
+    }
+}
